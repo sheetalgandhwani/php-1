c2eb38b9f76865b38a7b0c26cf7470922463e9af
Sheetal Prajapati <sheetalgandhwani777@gmail.com>
2024-03-05 01:42:06 +0530

    average.php

---
+++ average.php
@@ -0,0 +1,12 @@
+<?php
+
+$n1=readline("enter number:\n");
+$n2=readline("enter number:\n");
+$n3=readline("enter number:\n");
+$n4=readline("enter number:\n");
+$n5=readline("enter number:\n");
+
+$avg=($n1+$n2+$n3+$n4+$n5)/5;
+
+echo"average of five number is:$avg";
+?>
